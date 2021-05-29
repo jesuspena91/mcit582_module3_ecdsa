10311a76c47815cab9c281eac488970301cc3c15
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-29 17:09:42 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -11,7 +11,7 @@ def sign(m):
 
 	#generate signature
 	#Your code here
-	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
+	r, s = ecdsa.sign(m, private_key, curve=curve.secp256k1, hashfunc=sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
