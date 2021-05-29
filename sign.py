6ea235e967e10fb17f19e78884d81f1b485d11df
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-29 16:56:00 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -7,8 +7,8 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	private_key = keys.gen_private_key(curve=ecdsa.SECP256k1)
-	public_key = keys.get_public_key(private_key, curve=ecdsa.SECP256k1)
+	private_key, public_key = keys.gen_keypair(curve.secp256k1)
+	# public_key = keys.get_public_key(private_key, curve=ecdsa.SECP256k1)
 
 	#generate signature
 	#Your code here
